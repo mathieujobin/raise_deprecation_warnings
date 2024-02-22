74909985e526460ddec57d3c5ce07ec659030262
Mathieu Jobin <mathieu@justbudget.com>
2024-02-22 17:01:07 +0900

    fix error message for activesupport 6.x

--- Gemfile
+++ Gemfile
@@ -5,10 +5,8 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in raise_deprecation_warnings.gemspec
 gemspec
 
-gem "activesupport", "~> #{ENV.fetch("ACTIVE_SUPPORT_VERSION", 7.1)}"
-
-# gem "debug" # , "~> 13.0"
-
+gem "activesupport", "~> #{ENV.fetch("ACTIVE_SUPPORT_VERSION", "6.0.0")}"
+gem "debug"
 gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 gem "rubocop", "~> 1.21"
--- spec/raise_deprecation_warnings_spec.rb
+++ spec/raise_deprecation_warnings_spec.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
-# require 'debug'
+require 'debug'
 RSpec.describe RaiseDeprecationWarnings do
   it "has a version number" do
     expect(RaiseDeprecationWarnings::VERSION).not_to be nil
   end
 
   it "raise an error on deprecation warning" do
+    error_message =
+      if ActiveSupport.version >= "7.0"
+        'Calling warn on ActiveSupport::Deprecation is deprecated and will be ' \
+          'removed from Rails (use your own Deprecation object instead)'
+      else
+        'Deprecated!'
+      end
     expect do
       ActiveSupport::Deprecation.warn("Deprecated!")
       # rescue Exception
       #   debugger
       #   raise
-    end.to raise_error(
-      Exception,
-      'Calling warn on ActiveSupport::Deprecation is deprecated and will be ' \
-      'removed from Rails (use your own Deprecation object instead)'
-    )
+    end.to raise_error(Exception, error_message)
   end
 end
