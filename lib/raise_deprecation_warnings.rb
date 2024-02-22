fd41d570cc6e0ff5f01ff6eebaa91a0c7fb89235
Mathieu Jobin <mathieu@justbudget.com>
2024-02-22 16:02:27 +0900

    empty gem created with bundle

---
+++ lib/raise_deprecation_warnings.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "raise_deprecation_warnings/version"
+
+module RaiseDeprecationWarnings
+  class Error < StandardError; end
+  # Your code goes here...
+end
